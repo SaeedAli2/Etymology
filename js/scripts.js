767510091b4fb143384f9988b03cd2b15d98ef93
SaeedAli2 <P16152854@my365.dmu.ac.uk>
2020-03-23 20:11:26 +0000

    added in javascript for button to scroll to top

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log("hello");
+
+const scrollUp = document.getElementById ("btnscroll");
+
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+    if (document.documentElement.scrollTop > 50) {
+      scrollUp.style.display = "block";
+    } else {
+      scrollUp.style.display = "none";
+    }
+  }
+
+  scrollUp.addEventListener("click", function () {
+    window.scrollTo(0, 0);
+  }
+  ) 

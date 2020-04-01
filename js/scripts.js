01d6f1dd7021c69352cc9ea0fb77dc3fa7c275d7
SaeedAli2 <P16152854@my365.dmu.ac.uk>
2020-04-01 17:16:16 +0100

    modified modal pop up and fixed positioning error

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 "use strict";
+
+/*scroll to top of page functions*/
+
 const scrollUp = document.getElementById ("btnscroll");
 
  window.onscroll = function() {scrollFunction()};
@@ -14,4 +17,45 @@ const scrollUp = document.getElementById ("btnscroll");
   scrollUp.addEventListener("click", function () {
     window.scrollTo(0, 0);
   }
-  ) 
+) 
+
+/*const helper functions for form*/
+
+const contactusForm = document.getElementsByClassName("contactusform");
+
+const userName = document.getElementById("name");
+
+const emailAddress = document.getElementById("email");
+
+const bookGenre = document.getElementById("genre");
+
+const enterDetails = document.getElementById("enterdetails")
+
+const btn = document.getElementById("contactusbutton");
+
+/*const helper functions for modal*/
+
+const modal = document.getElementById("modalscreen");
+
+const span = document.getElementsByClassName("exitmodal")[0];
+
+/*modal onclick functions*/
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
